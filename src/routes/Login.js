fb34db4ba473fcdd92e68b4a9b0c34b7628e789a
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-02-10 12:32:32 -0600

    Rendering login errors to the user

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Message, Button, Input, Container, Header } from 'semantic-ui-react'
+import { Form, Message, Button, Input, Container, Header } from 'semantic-ui-react'
 import { extendObservable } from 'mobx';
 import { observer } from 'mobx-react';
 import { graphql } from 'react-apollo'
@@ -14,6 +14,7 @@ class Login extends React.Component {
         extendObservable(this, {
             email: '',
             password: '',
+            errors: {}
         })
     }
 
@@ -24,44 +25,85 @@ class Login extends React.Component {
             variables: { email, password }
         })
         console.log(response)
+
         /*Accessing token and refreshToken from the response*/
-        const { ok, token, refreshToken } = response.data.login
-        /*Storing the token and refreshToken in the user's local storage*/
-        localStorage.setItem = ('token', token)
-        localStorage.setItem = ('refreshToken', refreshToken)
+        const { ok, token, refreshToken, errors } = response.data.login
+
+        /*Store the token and refreshToken in the user's local storage*/
+        if (ok) {
+            localStorage.setItem = ('token', token)
+            localStorage.setItem = ('refreshToken', refreshToken)
+
+            /* if ok(true) redirect to homepage*/
+            this.props.history.push('/')
+        }
+        else {
+            const err = {};
+            /*looping through each path and message in errors*/
+            errors.forEach(({ path, message }) => {
+                /*err[passwordError] = message*/
+                err[`${path}Error`] = message
+            })
+            console.log('err', err)
+            this.errors = err
+        }
+        console.log(response)
     }
 
-    onChange = e => {
+    onChange = (e) => {
         /*taking the user's values from the inputs*/
         const { name, value } = e.target;
         this[name] = value;
     }
 
     render() {
-        /*using 'this' rather than 'this.state'*/
-        const { email, password } = this;
+        /*Getting the constructor*/
+        const { email, password, errors: { emailError, passwordError } } = this;
+        /*Hold errors*/
+        const errorList = []
+        /*If error onSubmit, push errors*/
+        if (emailError) {
+            errorList.push(emailError)
+        }
+        if (passwordError) {
+            errorList.push(passwordError)
+        }
 
         return (
 
             <Container text>
-                <Header as='h2'>Login</Header>
-                <Input
-                    name='email'
-                    onChange={this.onChange}
-                    value={email}
-                    placeholder='Email'
-                    fluid
-                />
-                <Input
-                    name='password'
-                    onChange={this.onChange}
-                    value={password}
-                    type='password'
-                    placeholder='Password'
-                    fluid
-                />
-                <Button onClick={this.onSubmit}>Submit</Button>
-            </Container>
+                <Form>
+                    <Header as='h2'>Login</Header>
+                    <Form.Field error={!!emailError}>
+                        <Input
+                            name='email'
+                            onChange={this.onChange}
+                            value={email}
+                            placeholder='Email'
+                            fluid
+                        />
+                    </Form.Field>
+                    <Form.Field error={!!passwordError}>
+                        <Input
+                            name='password'
+                            onChange={this.onChange}
+                            value={password}
+                            type='password'
+                            placeholder='Password'
+                            fluid
+                        />
+                    </Form.Field>
+                    <Button onClick={this.onSubmit}>Submit</Button>
+                </Form>
+                {/* Show Message only if errors */}
+                {errorList.length ? (
+                    <Message
+                        error
+                        header="There was an error with your submission"
+                        list={errorList}
+                    />
+                ) : null}
+            </Container >
         )
     }
 }
